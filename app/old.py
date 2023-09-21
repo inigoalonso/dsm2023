8127b12626edcd340edadb23136f3841dd7fa210
Iñigo Alonso Fernandez <inigoalonso@inigoalonso.com>
2023-09-21 10:13:15 +0200

    old stuff

---
+++ app/old.py
@@ -0,0 +1,28 @@
+
+# Add a logo
+# def add_logo():
+#     # https://discuss.streamlit.io/t/put-logo-and-title-above-on-top-of-page-navigation-in-sidebar-of-multipage-app/28213/4
+#     st.markdown(
+#         """
+#         <style>
+#             [data-testid="stHeader"]::before {
+#                 content: "Industry Sprint Workshop";
+#                 margin-left: 20px;
+#                 margin-top: 20px;
+#                 font-size: 30px;
+#                 position: relative;
+#             }
+#         </style>
+#         """,
+#         unsafe_allow_html=True,
+#     )
+
+# add_logo()
+
+# from utils import set_bg
+# set_bg('assets/background.png')
+
+####################
+# Sidebar          #
+
+# st.sidebar.header("Navigation")
