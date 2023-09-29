13d3395120fc9acccf2aaa142cf0c94eaefd884f
Iñigo Alonso Fernandez <inigoalonso@inigoalonso.com>
2023-09-29 13:44:38 +0200

    matrices plots

--- app/main.py
+++ app/main.py
@@ -119,7 +119,8 @@ pd.options.mode.chained_assignment = None  # default='warn'
 
 
 def on_data_update(data):
-    print("Data updated (not uploaded):", data)
+    #print("Data updated (not uploaded):", data)
+    pass
 
 
 def get_session_id() -> str:
@@ -901,13 +902,15 @@ if (group != "Select") and consent:
                 {
                     'plot_bgcolor': 'rgba(0, 0, 0, 0)',
                     'paper_bgcolor': 'rgba(0, 0, 0, 0)',
+                    'xaxis':{'fixedrange':True},
+                    'yaxis':{'fixedrange':True}
                 }
             )
 
             st.plotly_chart(
                 fig, 
                 use_container_width=True,
-                config={"displayModeBar": True,
+                config={"displayModeBar": False,
                 "displaylogo": False,
                 'modeBarButtonsToRemove': [
                     'zoom2d',
@@ -916,68 +919,14 @@ if (group != "Select") and consent:
                     'resetScale2d',
                     'toggleSpikelines',
                     'pan2d',
+                    'lasso2d',
+                    'select2d',
                     'autoScale2d',
                     'hoverClosestCartesian',
                     'hoverCompareCartesian'],
-                'plot_bgcolor':'red'}
+                }
             )
 
-            # tab_matrices_1, tab_matrices_2, tab_matrices_3 = st.tabs(
-            #     [
-            #         "DSM",
-            #         "Distance matrix",
-            #         "Risk matrix",
-            #     ]
-            # )
-
-            # with tab_matrices_1:
-            #     st.write("DSM")
-            #     st.image("assets/dsm.png", width=600)
-            # with tab_matrices_2:
-            #     st.write("Distance matrix")
-            #     st.image("assets/distance.png", width=600)
-            # with tab_matrices_3:
-            #     st.write("Risk matrix")
-            #     st.image("assets/risk.png", width=600)
-
-            # df_test = pd.DataFrame({"foo": [1, 2, 3], "bar": [4, 5, 6]})
-            # styler = df_test.style.background_gradient(cmap=cm_g2r)
-            # styler.set_table_styles([
-            #     {"selector": "tr", "props": "line-height: 10px;"},
-            #     {"selector": "td,th", "props": "line-height: inherit; padding: 0;"}
-            # ])
-            # html = styler.to_html()
-            # st.write(html, unsafe_allow_html=True)
-
-            # df_s1_dsm = pd.read_csv("data/s1_dsm.csv", sep=";", header=None, decimal=",").fillna(0)
-            # df_s2_dsm = pd.read_csv("data/s2_dsm.csv", sep=";", header=None, decimal=",").fillna(0)
-            # df_s3_dsm = pd.read_csv("data/s3_dsm.csv", sep=";", header=None, decimal=",").fillna(0)
-            # df_s1_distances = pd.read_csv("data/s1_distances.csv", sep=";", header=None, decimal=",").fillna(0)
-            # df_s2_distances = pd.read_csv("data/s2_distances.csv", sep=";", header=None, decimal=",").fillna(0)
-            # df_s3_distances = pd.read_csv("data/s3_distances.csv", sep=";", header=None, decimal=",").fillna(0)
-
-            # styler = df_s1_distances.style.background_gradient(cmap=cm_g2r)
-            # styler.set_table_styles([
-            #     {"selector": "tr", "props": "line-height: 12px;"},
-            #     {"selector": "td,th", "props": "line-height: inherit; padding: 0;"}
-            # ])
-            # html = styler.to_html()
-
-            # if system == "System 1 - Only front steering":
-            #     st.write("System 1 - Only front steering")
-            #     st.dataframe(df_s1_dsm)
-            #     st.write(html, unsafe_allow_html=True)
-            #     #st.dataframe(df_s1_distances.style.background_gradient(cmap=cm_r2g).format({2: '{:.2f}'}, na_rep='MISS', precision=2))
-            # elif system == "System 2 - Front + Back steering (hydraulic)":
-            #     st.write("System 2 - Front + Back steering (hydraulic)")
-            #     st.dataframe(df_s2_dsm)
-            #     st.dataframe(df_s2_distances.style.background_gradient(cmap=cm_r2g).format({2: '{:.2f}'}, na_rep='MISS', precision=2))
-            # elif system == "System 3 - Front + Back steering (electric)":
-            #     st.write("System 3 - Front + Back steering (electric)")
-            #     st.dataframe(df_s3_dsm)
-            #     st.dataframe(df_s3_distances.style.background_gradient(cmap=cm_r2g).format({2: '{:.2f}'}, na_rep='MISS', precision=2))
-            # else:
-            #     n = 10
 
         questions_tab2 = st.expander("Questions", expanded=True)
 
@@ -1717,8 +1666,8 @@ if (group != "Select") and consent:
         ]
     )
 
-print("Here's the session state:")
-print([key for key in st.session_state.keys()])
+#print("Here's the session state:")
+#print([key for key in st.session_state.keys()])
 # ic(st.session_state["data_editor"])
 
 try:
